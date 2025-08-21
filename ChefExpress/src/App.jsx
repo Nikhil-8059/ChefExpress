1149ef01451f417d5498c7b4e807f6b5afde9154
Nikhil-8059 <monu31598@gmail.com>
2025-08-21 23:54:56 +0530

    app

--- ChefExpress/src/App.jsx
+++ ChefExpress/src/App.jsx
@@ -5,12 +5,16 @@ import Contact from './Components/Contact/Contact'
 import Home from './Components/Home/home'
 import Head from './Components/Header/Head'
 import Footer from './Components/Footer/Footer'
+import Navbar from './Components/Header/navbar'
+
 
 
 const App = () => {
   return (
     <div>
+      
     <Head/>
+    <Navbar/>
       <Routes>
        <Route path='/' element={<Home/>}></Route>
         <Route path='/About' element={<About/>}/>
