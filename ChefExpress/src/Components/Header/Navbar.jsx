add3823fec7ce663f1e0fd65f9c50e21c809e27d
Nikhil-8059 <monu31598@gmail.com>
2025-08-27 23:22:09 +0530

    hamburger issue

--- ChefExpress/src/Components/Header/Navbar.jsx
+++ ChefExpress/src/Components/Header/Navbar.jsx
@@ -86,7 +86,7 @@ const Navbar = () => {
 
       {/* Slide-in Sidebar for desktop */}
       <div
-        className={`fixed top-0 right-0 h-full w-1/2 bg-white text-black shadow-lg z-50 transform transition-transform duration-300 ease-in-out ${
+        className={`fixed top-0 right-0 h-full w-1/3 bg-slate-900 bg-opacity-90 text-white shadow-lg z-50 transform transition-transform duration-300 ease-in-out ${
           isMenuOpen && isDesktop ? "translate-x-0" : "translate-x-full"
         }`}
       >
