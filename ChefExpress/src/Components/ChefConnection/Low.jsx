8709c70bcf17a53bb112b3557e36bcd3cc118bd9
Nikhil-8059 <monu31598@gmail.com>
2025-09-01 00:32:27 +0530

    fixed image

--- ChefExpress/src/Components/ChefConnection/Low.jsx
+++ ChefExpress/src/Components/ChefConnection/Low.jsx
@@ -3,21 +3,22 @@ import React from 'react'
 const Low = () => {
   return (
     <div>
-         <section className="text-black bg-zinc-500 body-font overflow-hidden">
-      <div className="container px-5 py-8">
+         <section className="text-black bg-neutral-500 body-font overflow-hidden">
+      <div className="container px-8 py-8">
         <div className="lg:w-4/5 flex flex-wrap">
           <img
             alt="Chefs"
-            className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
+            className="lg:w-1/2 w-full lg:h-auto h-full object-cover object-center rounded-md"
             src="https://github.com/Nikhil-8059/ChefExpress/blob/main/ChefExpress/public/images/7be9b858-1468-46c3-b1ea-7df98a648efd.png?raw=true"
           />
           <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-10">
             <h2 className="text-2xl title-font text-orange-400 font-bold tracking-normal">
               हमें है विश्वास
             </h2>
-            <h1 className="text-gray-900 text-justify text-6xl title-font font-extrabold mb-1">
-              आपका बनाया खाना, खाएगा ज़माना!
-            </h1>
+            <h1 className="text-gray-900 text-3xl sm:text-4xl lg:text-6xl title-font font-extrabold mb-1">
+  आपका बनाया खाना, खाएगा ज़माना!
+</h1>
+
 
             <h2 className="mt-8 text-xl title-font text-black font-bold tracking-normal">
               ऊपर जाएँ
