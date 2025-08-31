0c784a1d5b11ef2dc2758d601157227f644d057c
Nikhil-8059 <monu31598@gmail.com>
2025-09-01 00:25:42 +0530

    section created

--- ChefExpress/src/Components/ChefConnection/ChefConnection.jsx
+++ ChefExpress/src/Components/ChefConnection/ChefConnection.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import FAQ2 from './Faq2'
 import Lower from './Lower'
+import Low from './Low'
 
 const ChefConnection = () => {
   return (
     <div>
+        <Low/>
         <Lower/>
         <FAQ2/>
     </div>
