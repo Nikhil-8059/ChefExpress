17c0294da74088c8ed4fef2a95571ee40820565d
Nikhil-8059 <monu31598@gmail.com>
2025-09-04 23:59:08 +0530

    card 2

--- ChefExpress/src/Components/ChefConnection/Cater.jsx
+++ ChefExpress/src/Components/ChefConnection/Cater.jsx
@@ -41,11 +41,9 @@ const Cater = () => {
                 alt="blog"
               />
               <div className="p-6">
-                <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
-                  CATEGORY
-                </h2>
+                
                 <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                  The 400 Blows
+                  सम्मान से काम
                 </h1>
                 <p className="leading-relaxed mb-3">
                   Photo booth fam kinfolk cold-pressed sriracha leggings
