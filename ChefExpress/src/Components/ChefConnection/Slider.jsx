f7cf27a5b3b73cdf283c26b0da065af6debbaa4b
Nikhil-8059 <monu31598@gmail.com>
2025-09-08 00:51:19 +0530

    update

--- ChefExpress/src/Components/ChefConnection/Slider.jsx
+++ ChefExpress/src/Components/ChefConnection/Slider.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Slider from "react-slick";
 
-export default function SimpleSlider() {
+export default function Slider() {
   var settings = {
     dots: true,
     infinite: true,
