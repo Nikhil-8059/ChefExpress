702d5a70920c9436d63610802695f8f9405ccc85
Nikhil-8059 <monu31598@gmail.com>
2025-09-11 22:53:18 +0530

    slider update

--- ChefExpress/src/Components/ChefConnection/Slider.jsx
+++ ChefExpress/src/Components/ChefConnection/Slider.jsx
@@ -8,27 +8,59 @@ export default function SimpleSlider() {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
+    slidesToShow: 2,      // Show 2 images at once
+    slidesToScroll: 2,    // Slide 2 images at once
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 768,  // On smaller screens show 1 image
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
-    <Slider {...settings}>
-      <div className="flex justify-center items-center h-64">
-        <img src="images/Screenshot 2025-09-10 225427.png" alt="" className="object-contain max-h-full" />
-      </div>
-      <div className="flex justify-center items-center h-64">
-        <img src="images/Screenshot 2025-09-10 225442.png" alt="" className="object-contain max-h-full" />
-      </div>
-      <div className="flex justify-center items-center h-64">
-        <img src="images/Screenshot 2025-09-10 225442.png" alt="" className="object-contain max-h-full" />
-      </div>
-      <div className="flex justify-center items-center h-64">
-        <img src="images/Screenshot 2025-09-10 225442.png" alt="" className="object-contain max-h-full" />
-      </div>
-      <div className="flex justify-center items-center h-64">
-        <img src="images/Screenshot 2025-09-10 225442.png" alt="" className="object-contain max-h-full" />
-      </div>
-    </Slider>
+    <div className="w-full py-10">
+      <Slider {...settings}>
+        <div className="flex justify-center items-center h-[400px]">
+          <img
+            src="images/Screenshot 2025-09-10 225427.png"
+            alt="Slide 1"
+            className="max-h-full max-w-full object-contain"
+          />
+        </div>
+        <div className="flex justify-center items-center h-[400px]">
+          <img
+            src="images/Screenshot 2025-09-10 225442.png"
+            alt="Slide 2"
+            className="max-h-full max-w-full object-contain"
+          />
+        </div>
+        <div className="flex justify-center items-center h-[400px]">
+          <img
+            src="images/Screenshot 2025-09-10 225442.png"
+            alt="Slide 3"
+            className="max-h-full max-w-full object-contain"
+          />
+        </div>
+        <div className="flex justify-center items-center h-[400px]">
+          <img
+            src="images/Screenshot 2025-09-10 225442.png"
+            alt="Slide 4"
+            className="max-h-full max-w-full object-contain"
+          />
+        </div>
+        <div className="flex justify-center items-center h-[400px]">
+          <img
+            src="images/Screenshot 2025-09-10 225442.png"
+            alt="Slide 5"
+            className="max-h-full max-w-full object-contain"
+          />
+        </div>
+      </Slider>
+    </div>
   );
 }
