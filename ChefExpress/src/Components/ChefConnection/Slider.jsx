7c8e734b44814bc2ddd6b92458637506ab869828
Nikhil-8059 <monu31598@gmail.com>
2025-09-11 23:07:48 +0530

    update

--- ChefExpress/src/Components/ChefConnection/Slider.jsx
+++ ChefExpress/src/Components/ChefConnection/Slider.jsx
@@ -3,17 +3,45 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+// Custom Next Arrow
+const NextArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <div
+      className="absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer text-2xl z-10"
+      onClick={onClick}
+    >
+      ➡️
+    </div>
+  );
+};
+
+// Custom Previous Arrow
+const PrevArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <div
+      className="absolute left-2 top-1/2 transform -translate-y-1/2 cursor-pointer text-2xl z-10"
+      onClick={onClick}
+    >
+      ⬅️
+    </div>
+  );
+};
+
 export default function SimpleSlider() {
   const settings = {
-    dots: true,
+    dots: false,          // Hide dots
+    arrows: true,         // Show arrows
     infinite: true,
     speed: 500,
     slidesToShow: 2,      // Show 2 images at once
-    slidesToScroll: 2,    // Slide 2 images at once
-    arrows: true,
+    slidesToScroll: 2,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
     responsive: [
       {
-        breakpoint: 768,  // On smaller screens show 1 image
+        breakpoint: 768, // On mobile, show 1 slide at a time
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
@@ -23,41 +51,41 @@ export default function SimpleSlider() {
   };
 
   return (
-    <div className="w-full py-10">
+    <div className="w-full py-10 relative">
       <Slider {...settings}>
-        <div className="flex justify-center items-center h-[400px]">
+        <div className="h-[400px] flex justify-center items-center">
           <img
             src="images/Screenshot 2025-09-10 225427.png"
             alt="Slide 1"
-            className="max-h-full max-w-full object-contain"
+            className="h-full w-auto object-contain"
           />
         </div>
-        <div className="flex justify-center items-center h-[400px]">
+        <div className="h-[400px] flex justify-center items-center">
           <img
             src="images/Screenshot 2025-09-10 225442.png"
             alt="Slide 2"
-            className="max-h-full max-w-full object-contain"
+            className="h-full w-auto object-contain"
           />
         </div>
-        <div className="flex justify-center items-center h-[400px]">
+        <div className="h-[400px] flex justify-center items-center">
           <img
             src="images/Screenshot 2025-09-10 225442.png"
             alt="Slide 3"
-            className="max-h-full max-w-full object-contain"
+            className="h-full w-auto object-contain"
           />
         </div>
-        <div className="flex justify-center items-center h-[400px]">
+        <div className="h-[400px] flex justify-center items-center">
           <img
             src="images/Screenshot 2025-09-10 225442.png"
             alt="Slide 4"
-            className="max-h-full max-w-full object-contain"
+            className="h-full w-auto object-contain"
           />
         </div>
-        <div className="flex justify-center items-center h-[400px]">
+        <div className="h-[400px] flex justify-center items-center">
           <img
             src="images/Screenshot 2025-09-10 225442.png"
             alt="Slide 5"
-            className="max-h-full max-w-full object-contain"
+            className="h-full w-auto object-contain"
           />
         </div>
       </Slider>
