d9458660e797f9d18d67250c87facd840f9d5dc8
Nikhil-8059 <monu31598@gmail.com>
2025-09-19 00:03:57 +0530

    update

--- ChefExpress/src/Components/ChefConnection/Stats.jsx
+++ ChefExpress/src/Components/ChefConnection/Stats.jsx
@@ -10,29 +10,29 @@ const StatsSection = () => {
         <div className="flex flex-wrap justify-around w-full">
           {/* Stats Card 1 */}
           <div className="relative flex flex-col items-center text-center p-6 border rounded-xl md:border-0">
-            {/* Watermark Icon (desktop only, shifted right) */}
-            <div className="hidden md:block absolute top-1/2 left-1/2 transform -translate-y-1/2 translate-x-6 opacity-25 text-8xl text-gray-500 pointer-events-none">
+            {/* Watermark Icon */}
+            <div className="hidden md:block absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-20 text-9xl text-gray-400 pointer-events-none">
               <FaUtensils />
             </div>
-            <div className="text-5xl font-bold text-black mb-2 relative z-10">3M+</div>
+            <div className="text-5xl font-extrabold text-black mb-2 relative z-10">3M+</div>
             <p className="text-gray-600 text-lg relative z-10">Meals cooked with love</p>
           </div>
 
           {/* Stats Card 2 */}
           <div className="relative flex flex-col items-center text-center p-6 border rounded-xl md:border-0">
-            <div className="hidden md:block absolute top-1/2 left-1/2 transform -translate-y-1/2 translate-x-6 opacity-25 text-8xl text-gray-500 pointer-events-none">
+            <div className="hidden md:block absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-20 text-9xl text-gray-400 pointer-events-none">
               <PiChefHatBold />
             </div>
-            <div className="text-5xl font-bold text-black mb-2 relative z-10">4500+</div>
+            <div className="text-5xl font-extrabold text-black mb-2 relative z-10">4500+</div>
             <p className="text-gray-600 text-lg relative z-10">Verified & Trained Cooks</p>
           </div>
 
           {/* Stats Card 3 */}
           <div className="relative flex flex-col items-center text-center p-6 border rounded-xl md:border-0">
-            <div className="hidden md:block absolute top-1/2 left-1/2 transform -translate-y-1/2 translate-x-6 opacity-25 text-8xl text-gray-500 pointer-events-none">
+            <div className="hidden md:block absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-20 text-9xl text-gray-400 pointer-events-none">
               <FaHouseUser />
             </div>
-            <div className="text-5xl font-bold text-black mb-2 relative z-10">10K+</div>
+            <div className="text-5xl font-extrabold text-black mb-2 relative z-10">10K+</div>
             <p className="text-gray-600 text-lg relative z-10">Households served</p>
           </div>
         </div>
