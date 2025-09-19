16920bed1a388d85e710c12b876de7105d2f6554
Nikhil-8059 <monu31598@gmail.com>
2025-09-20 01:49:04 +0530

    update

--- ChefExpress/src/Components/ChefConnection/Stats.jsx
+++ ChefExpress/src/Components/ChefConnection/Stats.jsx
@@ -6,30 +6,49 @@ const StatsSection = () => {
     <section className="text-gray-600 body-font">
       <div className="container mx-auto flex justify-center py-8 px-4 md:px-0">
         <div className="flex flex-row justify-evenly w-full gap-3 md:gap-1">
+
           {/* Card 1 */}
-          <div className="flex flex-col items-center text-center p-4 border rounded-xl md:border-0 md:rounded-none">
-            <div className="flex items-center">
-              <div className="text-xl md:text-5xl font-bold text-black">3M+</div>
-              <IoRestaurant className="text-600 text-lg opacity-30 md:text-2xl ml-1" />
-            </div>
-            <p className="text-gray-600 text-xs md:text-lg mt-1">Meals cooked with love</p>
+          <div className="relative flex flex-col md:items-center items-start text-left md:text-center p-4 border rounded-xl md:border-1 md:rounded-none">
+            <span className="text-xl md:text-5xl font-bold text-black z-10 relative">
+              3M+
+            </span>
+            <p className="text-gray-600 text-xs md:text-lg mt-1 z-10 relative">
+              Meals cooked with love
+            </p>
+            <IoRestaurant
+              className="hidden md:block absolute -right-5 top-2 md:top-4 md:-right-6 text-black opacity-10 text-7xl md:text-8xl z-0 pointer-events-none select-none"
+              aria-hidden="true"
+            />
           </div>
+
           {/* Card 2 */}
-          <div className="flex flex-col items-center text-center p-4 border rounded-xl md:border-0 md:rounded-none">
-            <div className="flex items-center">
-              <div className="text-xl md:text-5xl font-bold text-black">4500+</div>
-              <IoPerson className="text-blue-600 text-lg md:text-2xl ml-1" />
-            </div>
-            <p className="text-gray-600 text-xs md:text-lg mt-1">Verified & Trained Cooks</p>
+          <div className="relative flex flex-col md:items-center items-start text-left md:text-center p-4 border rounded-xl md:border-1 md:rounded-none">
+            <span className="text-xl md:text-5xl font-bold text-black z-10 relative">
+              4500+
+            </span>
+            <p className="text-gray-600 text-xs md:text-lg mt-1 z-10 relative">
+              Verified & Trained Cooks
+            </p>
+            <IoPerson
+              className="hidden md:block absolute -right-5 top-2 md:top-4 md:-right-6 text-black opacity-10 text-7xl md:text-8xl z-0 pointer-events-none select-none"
+              aria-hidden="true"
+            />
           </div>
+
           {/* Card 3 */}
-          <div className="flex flex-col items-center text-center p-4 border rounded-xl md:border-0 md:rounded-none">
-            <div className="flex items-center">
-              <div className="text-xl md:text-5xl font-bold text-black">10K+</div>
-              <IoHomeOutline className="text-orange-600 text-lg md:text-2xl ml-1" />
-            </div>
-            <p className="text-gray-600 text-xs md:text-lg mt-1">Households served</p>
+          <div className="relative flex flex-col md:items-center items-start text-left md:text-center p-4 border rounded-xl md:border-1 md:rounded-none">
+            <span className="text-xl md:text-5xl font-bold text-black z-10 relative">
+              10K+
+            </span>
+            <p className="text-gray-600 text-xs md:text-lg mt-1 z-10 relative">
+              Households served
+            </p>
+            <IoHomeOutline
+              className="hidden md:block absolute -right-5 top-2 md:top-4 md:-right-6 text-black opacity-10 text-7xl md:text-8xl z-0 pointer-events-none select-none"
+              aria-hidden="true"
+            />
           </div>
+
         </div>
       </div>
     </section>
