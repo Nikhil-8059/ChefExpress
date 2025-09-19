33bf1fa8f97f016c8d1c9656aa7498ff2d424694
Nikhil-8059 <monu31598@gmail.com>
2025-09-20 01:47:18 +0530

    update

--- ChefExpress/src/Components/ChefConnection/Stats.jsx
+++ ChefExpress/src/Components/ChefConnection/Stats.jsx
@@ -8,7 +8,7 @@ const StatsSection = () => {
       <div className="container mx-auto flex justify-center py-8 px-4 md:px-0">
         <div className="flex flex-row justify-around w-full gap-3 md:gap-1">
           {/* Card 1 */}
-          <div className="flex flex-col items-center text-center p-4 flex-1
+          <div className="flex flex-col items-center text-center p-4 
                           border rounded-xl md:border-1 md:rounded-none">
             <div className="flex items-center">
               <div className="text-xl md:text-3xl font-bold text-black">3M+</div>
@@ -20,8 +20,8 @@ const StatsSection = () => {
           </div>
 
           {/* Card 2 */}
-          <div className="flex flex-col items-center text-center p-4 flex-1
-                          border rounded-xl md:border-0 md:rounded-none">
+          <div className="flex flex-col items-center text-center p-4 
+                          border rounded-xl md:border-1 md:rounded-none">
             <div className="flex items-center">
               <div className="text-xl md:text-3xl font-bold text-black">4500+</div>
               <PiChefHatBold className="text-blue-600 text-lg md:text-2xl ml-1" />
@@ -33,7 +33,7 @@ const StatsSection = () => {
 
           {/* Card 3 */}
           <div className="flex flex-col items-center text-center p-4 flex-1
-                          border rounded-xl md:border-0 md:rounded-none">
+                          border rounded-xl md:border-1 md:rounded-none">
             <div className="flex items-center">
               <div className="text-xl md:text-3xl font-bold text-black">10K+</div>
               <FaHome className="text-orange-600 text-lg md:text-2xl ml-1" />
